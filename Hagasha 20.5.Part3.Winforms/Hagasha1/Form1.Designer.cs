0adde9eac954d8e39f46a5567a66c84ed8ca1c0e
MichaelBakshi <62958898+MichaelBakshi@users.noreply.github.com>
2020-06-10 18:15:29 +0300

    Add files via upload

---
+++ Hagasha 20.5.Part3.Winforms/Hagasha1/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace Hagasha1
+{
+    partial class RememberTheNumbers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.FirstLbl = new System.Windows.Forms.Label();
+            this.SecondLbl = new System.Windows.Forms.Label();
+            this.ThirdLbl = new System.Windows.Forms.Label();
+            this.FourthLbl = new System.Windows.Forms.Label();
+            this.FifthLbl = new System.Windows.Forms.Label();
+            this.FirstTxtBox = new System.Windows.Forms.TextBox();
+            this.SecondTxtBox = new System.Windows.Forms.TextBox();
+            this.ThirdTxtBox = new System.Windows.Forms.TextBox();
+            this.FourthTxtBox = new System.Windows.Forms.TextBox();
+            this.FifthTxtBox = new System.Windows.Forms.TextBox();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.FinishBtn = new System.Windows.Forms.Button();
+            this.FirstListBox = new System.Windows.Forms.ListBox();
+            this.ScoresLbl = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(579, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // gameToolStripMenuItem
+            // 
+            this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.gameToolStripMenuItem.Text = "&Game";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.newGameToolStripMenuItem.Text = "&New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.exitToolStripMenuItem.Text = "&Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // FirstLbl
+            // 
+            this.FirstLbl.AutoSize = true;
+            this.FirstLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FirstLbl.Location = new System.Drawing.Point(42, 74);
+            this.FirstLbl.Name = "FirstLbl";
+            this.FirstLbl.Size = new System.Drawing.Size(51, 16);
+            this.FirstLbl.TabIndex = 1;
+            this.FirstLbl.Text = "label1";
+            this.FirstLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // SecondLbl
+            // 
+            this.SecondLbl.AutoSize = true;
+            this.SecondLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SecondLbl.Location = new System.Drawing.Point(138, 77);
+            this.SecondLbl.Name = "SecondLbl";
+            this.SecondLbl.Size = new System.Drawing.Size(51, 16);
+            this.SecondLbl.TabIndex = 2;
+            this.SecondLbl.Text = "label2";
+            this.SecondLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ThirdLbl
+            // 
+            this.ThirdLbl.AutoSize = true;
+            this.ThirdLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ThirdLbl.Location = new System.Drawing.Point(240, 77);
+            this.ThirdLbl.Name = "ThirdLbl";
+            this.ThirdLbl.Size = new System.Drawing.Size(51, 16);
+            this.ThirdLbl.TabIndex = 3;
+            this.ThirdLbl.Text = "label3";
+            this.ThirdLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FourthLbl
+            // 
+            this.FourthLbl.AutoSize = true;
+            this.FourthLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FourthLbl.Location = new System.Drawing.Point(349, 76);
+            this.FourthLbl.Name = "FourthLbl";
+            this.FourthLbl.Size = new System.Drawing.Size(51, 16);
+            this.FourthLbl.TabIndex = 4;
+            this.FourthLbl.Text = "label4";
+            this.FourthLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FifthLbl
+            // 
+            this.FifthLbl.AutoSize = true;
+            this.FifthLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FifthLbl.Location = new System.Drawing.Point(464, 77);
+            this.FifthLbl.Name = "FifthLbl";
+            this.FifthLbl.Size = new System.Drawing.Size(51, 16);
+            this.FifthLbl.TabIndex = 5;
+            this.FifthLbl.Text = "label5";
+            this.FifthLbl.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // FirstTxtBox
+            // 
+            this.FirstTxtBox.Location = new System.Drawing.Point(45, 141);
+            this.FirstTxtBox.Name = "FirstTxtBox";
+            this.FirstTxtBox.Size = new System.Drawing.Size(48, 20);
+            this.FirstTxtBox.TabIndex = 6;
+            // 
+            // SecondTxtBox
+            // 
+            this.SecondTxtBox.Location = new System.Drawing.Point(141, 141);
+            this.SecondTxtBox.Name = "SecondTxtBox";
+            this.SecondTxtBox.Size = new System.Drawing.Size(48, 20);
+            this.SecondTxtBox.TabIndex = 7;
+            // 
+            // ThirdTxtBox
+            // 
+            this.ThirdTxtBox.Location = new System.Drawing.Point(243, 141);
+            this.ThirdTxtBox.Name = "ThirdTxtBox";
+            this.ThirdTxtBox.Size = new System.Drawing.Size(48, 20);
+            this.ThirdTxtBox.TabIndex = 8;
+            // 
+            // FourthTxtBox
+            // 
+            this.FourthTxtBox.Location = new System.Drawing.Point(352, 141);
+            this.FourthTxtBox.Name = "FourthTxtBox";
+            this.FourthTxtBox.Size = new System.Drawing.Size(48, 20);
+            this.FourthTxtBox.TabIndex = 9;
+            // 
+            // FifthTxtBox
+            // 
+            this.FifthTxtBox.Location = new System.Drawing.Point(467, 141);
+            this.FifthTxtBox.Name = "FifthTxtBox";
+            this.FifthTxtBox.Size = new System.Drawing.Size(48, 20);
+            this.FifthTxtBox.TabIndex = 10;
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.BackColor = System.Drawing.Color.YellowGreen;
+            this.StartBtn.Location = new System.Drawing.Point(115, 235);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(101, 37);
+            this.StartBtn.TabIndex = 11;
+            this.StartBtn.Text = "&Start";
+            this.StartBtn.UseVisualStyleBackColor = false;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // FinishBtn
+            // 
+            this.FinishBtn.BackColor = System.Drawing.Color.LightPink;
+            this.FinishBtn.Location = new System.Drawing.Point(325, 235);
+            this.FinishBtn.Name = "FinishBtn";
+            this.FinishBtn.Size = new System.Drawing.Size(103, 37);
+            this.FinishBtn.TabIndex = 12;
+            this.FinishBtn.Text = "&Finish";
+            this.FinishBtn.UseVisualStyleBackColor = false;
+            this.FinishBtn.Click += new System.EventHandler(this.FinishBtn_Click);
+            // 
+            // FirstListBox
+            // 
+            this.FirstListBox.FormattingEnabled = true;
+            this.FirstListBox.Location = new System.Drawing.Point(115, 332);
+            this.FirstListBox.Name = "FirstListBox";
+            this.FirstListBox.Size = new System.Drawing.Size(313, 173);
+            this.FirstListBox.TabIndex = 13;
+            // 
+            // ScoresLbl
+            // 
+            this.ScoresLbl.AutoSize = true;
+            this.ScoresLbl.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ScoresLbl.Location = new System.Drawing.Point(115, 300);
+            this.ScoresLbl.Name = "ScoresLbl";
+            this.ScoresLbl.Size = new System.Drawing.Size(76, 13);
+            this.ScoresLbl.TabIndex = 14;
+            this.ScoresLbl.Text = "&High Scores";
+            // 
+            // RememberTheNumbers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 534);
+            this.Controls.Add(this.ScoresLbl);
+            this.Controls.Add(this.FirstListBox);
+            this.Controls.Add(this.FinishBtn);
+            this.Controls.Add(this.StartBtn);
+            this.Controls.Add(this.FifthTxtBox);
+            this.Controls.Add(this.FourthTxtBox);
+            this.Controls.Add(this.ThirdTxtBox);
+            this.Controls.Add(this.SecondTxtBox);
+            this.Controls.Add(this.FirstTxtBox);
+            this.Controls.Add(this.FifthLbl);
+            this.Controls.Add(this.FourthLbl);
+            this.Controls.Add(this.ThirdLbl);
+            this.Controls.Add(this.SecondLbl);
+            this.Controls.Add(this.FirstLbl);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "RememberTheNumbers";
+            this.Text = "Remember the numbers!";
+            this.Load += new System.EventHandler(this.RememberTheNumbers_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Label FirstLbl;
+        private System.Windows.Forms.Label SecondLbl;
+        private System.Windows.Forms.Label ThirdLbl;
+        private System.Windows.Forms.Label FourthLbl;
+        private System.Windows.Forms.Label FifthLbl;
+        private System.Windows.Forms.TextBox FirstTxtBox;
+        private System.Windows.Forms.TextBox SecondTxtBox;
+        private System.Windows.Forms.TextBox ThirdTxtBox;
+        private System.Windows.Forms.TextBox FourthTxtBox;
+        private System.Windows.Forms.TextBox FifthTxtBox;
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.Button FinishBtn;
+        private System.Windows.Forms.ListBox FirstListBox;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label ScoresLbl;
+    }
+}
+
---
+++ Hagasha 20.5.Part3.Winforms/Hagasha1/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hagasha1
+{
+    public partial class RememberTheNumbers : Form
+    {
+        public RememberTheNumbers()
+        {
+            InitializeComponent();
+        }
+
+        Random generator = new Random();
+        const int upperBoundOfRandom = 100;
+        Stopwatch sw = new Stopwatch();
+
+        private void RememberTheNumbers_Load(object sender, EventArgs e)
+        {
+            FirstLbl.Text = generator.Next(1, upperBoundOfRandom + 1).ToString();
+            SecondLbl.Text = generator.Next(1, upperBoundOfRandom + 1).ToString();
+            ThirdLbl.Text = generator.Next(1, upperBoundOfRandom + 1).ToString();
+            FourthLbl.Text = generator.Next(1, upperBoundOfRandom + 1).ToString();
+            FifthLbl.Text = generator.Next(1, upperBoundOfRandom + 1).ToString();
+
+            FirstTxtBox.Enabled = false;
+            SecondTxtBox.Enabled = false;
+            ThirdTxtBox.Enabled = false;
+            FourthTxtBox.Enabled = false;
+            FifthTxtBox.Enabled = false;
+
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            FirstLbl.Visible = false;
+            SecondLbl.Visible = false;
+            ThirdLbl.Visible = false;
+            FourthLbl.Visible = false;
+            FifthLbl.Visible = false;
+
+            FirstTxtBox.Enabled = true;
+            SecondTxtBox.Enabled = true;
+            ThirdTxtBox.Enabled = true;
+            FourthTxtBox.Enabled = true;
+            FifthTxtBox.Enabled = true;
+
+            FirstListBox.Items.Clear();
+
+            //sw.Stop();
+            sw.Start();
+
+        }
+
+        private void FinishBtn_Click(object sender, EventArgs e)
+        {
+            int scoreForRound = 0;
+           
+
+            if (FirstLbl.Text == FirstTxtBox.Text)
+            {
+                scoreForRound++;
+            }
+            else
+            {
+                FirstTxtBox.ForeColor = Color.Red;
+            }
+
+            if (SecondLbl.Text == SecondTxtBox.Text)
+            {
+                scoreForRound++;
+            }
+            else
+            {
+                SecondTxtBox.ForeColor = Color.Red;
+            }
+
+            if (ThirdLbl.Text == ThirdTxtBox.Text)
+            {
+                scoreForRound++;
+            }
+            else
+            {
+                ThirdTxtBox.ForeColor = Color.Red;
+            }
+
+            if (FourthLbl.Text == FourthTxtBox.Text)
+            {
+                scoreForRound++;
+            }
+            else
+            {
+                FourthTxtBox.ForeColor = Color.Red;
+            }
+
+            if (FifthLbl.Text == FifthTxtBox.Text)
+            {
+                scoreForRound++;
+            }
+            else
+            {
+                FifthTxtBox.ForeColor = Color.Red;
+            }
+
+            FirstListBox.Items.Add(scoreForRound);
+
+
+            if (FirstLbl.Text == FirstTxtBox.Text && SecondLbl.Text == SecondTxtBox.Text && ThirdLbl.Text == ThirdTxtBox.Text && FourthLbl.Text == FourthTxtBox.Text && FifthLbl.Text == FifthTxtBox.Text)
+            {
+                MessageBox.Show($"You won 100% ! And it took you only { sw.ElapsedMilliseconds/1000.0} seconds!");
+            }
+
+            else
+            {
+                MessageBox.Show($"Your score is {scoreForRound}! It took you {sw.ElapsedMilliseconds/1000.0} seconds! Try again!");
+            }
+            sw.Stop();
+
+
+        }
+
+
+        //new game
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FirstLbl.Text = generator.Next(1, upperBoundOfRandom + 1).ToString();
+            SecondLbl.Text = generator.Next(1, upperBoundOfRandom + 1).ToString();
+            ThirdLbl.Text = generator.Next(1, upperBoundOfRandom + 1).ToString();
+            FourthLbl.Text = generator.Next(1, upperBoundOfRandom + 1).ToString();
+            FifthLbl.Text = generator.Next(1, upperBoundOfRandom + 1).ToString();
+
+            FirstListBox.Items.Clear();
+            
+            FirstTxtBox.Clear();
+            SecondTxtBox.Clear();
+            ThirdTxtBox.Clear();
+            FourthTxtBox.Clear();
+            FifthTxtBox.Clear();
+        }
+
+        //exit game
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result =  MessageBox.Show("Are you sure you want to leave?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+    }
+}
